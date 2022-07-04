ec8922302b4f5022d9081e33b36e3dfa40d8b81b
Shell Raiser <ksshailesh05@gmail.com>
2022-07-04 11:35:26 +0530

    console log demo of onAuthStateChanged()

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import {initializeApp} from 'firebase/app'
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyD5jD7cFbhxmVFdZVAHOs6bhs_G--vQfUw",
-  authDomain: "fir-frontend-7cbae.firebaseapp.com",
-  projectId: "fir-frontend-7cbae",
-  storageBucket: "fir-frontend-7cbae.appspot.com",
-  messagingSenderId: "285606309154",
-  appId: "1:285606309154:web:4127ca1c16fa6a818a097b",
-  measurementId: "G-B8WL5Z63D5"
+    authDomain: "fir-frontend-7cbae.firebaseapp.com",
+    projectId: "fir-frontend-7cbae",
+    storageBucket: "fir-frontend-7cbae.appspot.com",
+    messagingSenderId: "285606309154",
+    appId: "1:285606309154:web:4127ca1c16fa6a818a097b",
+    measurementId: "G-B8WL5Z63D5"
 })
 
 // Initialize Firebase
-const analytics = getAnalytics(firebaseApp);
+
+const auth = getAuth(firebaseApp);
+
+
+// Detect auth state
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        console.log('logged in!');
+    } else {
+        console.log('No user');
+    }
+});
+
